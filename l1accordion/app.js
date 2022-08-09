3d17c2b75ca9c46453b5f5cdbbf608f4eb3fc318
hein khant <heinkhantzaw14720@gmail.com>
2022-08-09 15:42:05 +0630

    l1accordion

---
+++ l1accordion/app.js
@@ -0,0 +1,30 @@
+var getacctitles = document.getElementsByClassName('acctitle');
+// console.log(getacctitles); //HTML Collection
+
+var getacccontents = document.querySelectorAll('.acccontent');
+// console.log(getacccontents); //NodeList
+
+for(var x = 0; x < getacctitles.length; x++){
+    getacctitles[x].addEventListener('click',function(e){
+        // e.target == this
+        // console.log(e.target); 
+        // console.log(this);
+       
+
+        this.classList.toggle('active');
+        var getcontent = this.nextElementSibling;
+        // console.log(getcontent);
+
+        if(getcontent.style.height){
+            // getcontent.style.height = 0; //-->beware can't set 0
+            getcontent.style.height = null;
+        }else{
+            // console.log(getcontent.scrollHeight);
+            getcontent.style.height = getcontent.scrollHeight + "px";
+        }
+    });
+
+    if(getacctitles[x].classList.contains('active')){
+        getacccontents[x].style.height = getacccontents[x].scrollHeight+ "px";
+    }
+}
